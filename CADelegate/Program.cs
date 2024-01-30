1f19168e0f1ddc7186afe383a3096104bcd27205
unknown <abdallahaliahmed517@gmail.com>
2024-01-30 20:29:27 +0200

    LLL

--- CADelegate/Program.cs
+++ CADelegate/Program.cs
@@ -1,1 +1,26 @@
-﻿
+﻿delegate void MDelegate();
+//delegate void MDelegate();
+//Action, Func, Pred
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        //M2(M1);
+        Action action = M1;
+        M2(action);
+    }
+    static void M1()
+    {
+        Console.WriteLine("M1");
+    }
+    //static void M2(MDelegate mDel)
+    //{
+    //    mDel();
+    //    Console.WriteLine("M2");
+    //}
+    static void M2(Action action)
+    {
+        action();
+        Console.WriteLine("M2");
+    }
+}
